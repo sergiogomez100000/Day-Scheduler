4c95be25829bdde9d445e925cc66f8e4d3aa88b8
Sergio Gomez <sergiogomez100000@gmail.com>
2021-03-06 19:41:20 -0700

    setting variables for all timeframes, creating function to save description

--- assests/script.js
+++ assests/script.js
@@ -10,18 +10,51 @@ currentDay.textContent = today;
 //WHEN I scroll down//THEN I am presented with timeblocks for standard business hours//WHEN I view the timeblocks for that day
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 var currentHour=moment().format("HH");//got military hour
-currentHour= currentHour;
 console.log(currentHour); //logd in military number
-console.log(typeof currentHour);
-var hour9 =document.querySelector("#hour-9");
-var hour9 = 9
-console.log(hour9);
-console.log(typeof hour9);
+var hour9 =document.querySelector("#hour-9");//assign variable to hour-9 id
+var hour9 = 9//assigns numerical value to variable hour9
+var hour10 = document.querySelector("#hour-10");//assign variable to hour-10 id
+var hour10 = 10//assigns numerical value to variable hour10
+var hour11= document.querySelector("#hour-11");//assign variable to hour-11 id
+var hour11=11//assigns numerical value to variable hour11
+var hour12 =document.querySelector("#hour-12");//assign variable to hour-12 id
+var hour12 = 12//assigns numerical value to variable hour12
+var hour13 =document.querySelector("#hour-13");//assign variable to hour-13 id
+var hour13 = 13//assigns numerical value to variable hour13
+var hour14 =document.querySelector("#hour-14");//assign variable to hour-14 id
+var hour14 = 14//assigns numerical value to variable hour14
+var hour15 =document.querySelector("#hour-15");//assign variable to hour-15 id
+var hour15 = 15//assigns numerical value to variable hour15
+var hour16 =document.querySelector("#hour-16");//assign variable to hour-16 id
+var hour16 = 16//assigns numerical value to variable hour16
+var hour17 =document.querySelector("#hour-17");//assign variable to hour-17 id
+var hour17 = 17//assigns numerical value to variable hour17
+//sets an array with all different containers as value for timeframe variable
+var timeframe = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17]
+
+// creates function to colorcode timeframes
+function colorCode(){
+	for(i= 0;i<timeframe.length;i++){
+	if(currentHour==timeframe[i])
+	timeframe[i].addclass(present);
+	if(currentHour<timeframe[i])
+	timeframe[i].addclass(future);
+	else	
+	timeframe[i].addclass(past);
+	
+
+}}
+
+
+
+
+
 
 //WHEN I click into a timeblock//THEN I can enter an event
 //WHEN I click the save button for that timeblock
 var savebutton = document.getElementsByClassName("btn saveBtn")
-var description= document.getElementsByClassName("col-md-10 description")
+var description= document.getElementsByClassName("description")
+function addDescription(){
 savebutton.addEventListener("click",function(){
     preventDefault();
     if (description.value.length < 1) return;
@@ -34,7 +67,7 @@ savebutton.addEventListener("click",function(){
 	localStorage.setItem('description', description.innerHTML);
 
 
-})
+})}
 //THEN the text for that event is saved in local storage
 //WHEN I refresh the page
 //THEN the saved events persist
